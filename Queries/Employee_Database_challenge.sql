fdd8de38695758a685b152b49a764aee5275a009
Erika L <erika.lewars@gmail.com>
2022-05-01 15:05:19 -0400

    Adding Challenge

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,35 @@
+SELECT ce.emp_no,
+  ce.first_name,
+  ce.last_name,
+  ti.title,
+  ti.from_date,
+  ti.to_date
+INTO ret_titles
+FROM employees AS ce
+  INNER JOIN titles AS ti
+    ON (ce.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY ce.emp_no;
+
+SELECT * FROM ret_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM ret_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT * FROM unique_titles
+
+-- Counting the number of each employee per title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles
+
+
+
